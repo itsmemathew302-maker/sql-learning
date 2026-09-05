0eab320a5c0e23ae8b2f1971bc7b49515b6bf0f3
itsmemathew302-maker <itsmemathew302@gmail.com>
2026-09-05 13:27:17 +0530

    Create 01-sql-basics.sql

---
+++ 01-sql-basics.sql
@@ -0,0 +1,12 @@
+CREATE TABLE student (
+    id INT, 
+    name VARCHAR(50),
+    age INT,
+    department VARCHAR(50)
+);
+
+INSERT INTO student VALUE
+(1, "prince", 20, "cse"),
+(2, "naruto", 17, "food tech"),
+(3, "hinata", 2, "not going to school");
+SELECT * FROM student;
